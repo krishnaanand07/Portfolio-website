c99d8c35611ef72dc1bca8cc33b0eccffb3ee4bd
candidatetohr <candidatetohr@gmail.com>
2026-07-16 00:18:08 +0530

    Add Vercel Analytics and Speed Insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 export default function App() {
   return (
     <MainLayout>
       <Home />
+      <Analytics />
+      <SpeedInsights />
     </MainLayout>
   );
 }
